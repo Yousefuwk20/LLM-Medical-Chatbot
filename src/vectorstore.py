8dd928f1cd66e1667e3f833fefb6c1ff7a76ea5b
Yousef Mohamed <112489749+Yousefuwk20@users.noreply.github.com>
2024-09-22 03:00:58 +0300

    Add vectorstore file

---
+++ src/vectorstore.py
@@ -0,0 +1,23 @@
+from langchain.embeddings import SentenceTransformerEmbeddings
+from langchain.vectorstores import Pinecone
+
+def setup_vectorstore(index):
+    """
+    Set up the vector store for retrieval using Pinecone and sentence embeddings.
+
+    Args:
+        index (Pinecone.Index): Initialized Pinecone index.
+        embed_model (SentenceTransformerEmbeddings): Model for embedding text queries.
+
+    Returns:
+        vectorstore (LangchainPinecone): A Pinecone-based vector store for retrieval.
+    """
+    text_field = 'text'
+    embed = SentenceTransformerEmbeddings(model_name="NeuML/pubmedbert-base-embeddings")
+
+    vectorstore = Pinecone(
+        index, 
+        embed.embed_query,
+        text_key=text_field
+    )
+    return vectorstore
